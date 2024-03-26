2205becf594e67db80ca3839f1a90b3adeb13106
MarcosFrMelo <marcos.freire@mail.uft.edu.br>
2024-03-26 18:25:45 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,6 +1,8 @@
+import java.util.Date;
+
 public class teste {
     public static void main(String[] args) {
-        Integer teste = 10;
+        Integer teste = 5;
         int teste2 = 80;
         teste.hashCode();
         teste.doubleValue();
@@ -10,7 +12,15 @@ public class teste {
         System.out.println(numeroNormal);
         short numeroCurto2 = (short) numeroNormal;
         System.out.println(numeroCurto2);
+        Date dataNascimento = new Date(1 -2-2024);
+        System.out.println(dataNascimento);
 
+        String concatenacao = "1"+1+1+1;
+        teste++;
+        System.out.println(teste++);
+        System.out.println(teste);
+        boolean eae = true;
+        System.out.println("inverteu?:" + !eae);
     }
 }
 
