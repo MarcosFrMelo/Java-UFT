cf42cf87c1602b0608cfc39f8dc01b4691120a86
MarcosFrMelo <marcos.freire@mail.uft.edu.br>
2024-03-26 17:58:47 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -2,6 +2,15 @@ public class teste {
     public static void main(String[] args) {
         Integer teste = 10;
         int teste2 = 80;
-        teste.compareTo(teste2);
+        teste.hashCode();
+        teste.doubleValue();
+        short numeroCurto = 1;
+        System.out.println(numeroCurto );
+        int numeroNormal = numeroCurto;
+        System.out.println(numeroNormal);
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
+
     }
 }
+
