0243d3b4bd17dc0d0ca827ea2e702911fe2b67ab
MarcosFrMelo <marcos.freire@mail.uft.edu.br>
2024-03-26 18:44:28 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -21,6 +21,18 @@ public class teste {
         System.out.println(teste);
         boolean eae = true;
         System.out.println("inverteu?:" + !eae);
+
+        String resultado = (teste==teste2) ? "verdadeiro" : "false";
+        System.out.println(resultado);
+
+        String nome1 = "JAVA";
+        String nome2 = "JAVA";
+        System.out.println(nome1.equals(nome2));
+        String nome3 = new String("JAVA");
+        //System.out.println(nome3);
+        //System.out.println(nome3.equals(nome1));
+        String nome4 = nome3;
+        System.out.println(nome4 == nome3);
     }
 }
 
