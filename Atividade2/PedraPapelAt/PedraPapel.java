90559f05d6808daf5ea70c0c5baf419d25f47672
MarcosFrMelo <143053350+MarcosFrMelo@users.noreply.github.com>
2024-04-09 20:14:41 -0300

    att

---
+++ Atividade2/PedraPapelAt/PedraPapel.java
@@ -0,0 +1,34 @@
+package Atividade2.PedraPapelAt;
+
+import java.util.Scanner;
+
+public class PedraPapel {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        String jogador1, jogador2;
+
+        System.out.println("Insira quantos jogos vcs irão realizar");
+        int n = scanner.nextInt();
+
+        for(int i = 0; i < n; i++){
+            
+            try{
+                System.out.println("Jogador 1, insira a opção que deseja(ataque, papel, pedra)");
+                jogador1 = scanner.next();
+
+                System.out.println("Jogador 2, insira a opção que deseja(ataque, papel, pedra)");
+                jogador2 = scanner.next();
+            } catch(Exception e){
+                System.out.println("As repostas devem ser escritas com ");
+            }
+
+
+
+        }
+
+
+    }
+    
+}
---
+++ Atividade3/Nomes/Nomes.java
@@ -0,0 +1,21 @@
+package Atividade3.Nomes;
+
+import java.util.Scanner;
+
+public class Nomes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] nomes = new String[10];
+        for(int i = 0; i < 10; i++){
+            nomes[i] = scanner.nextLine();
+        }
+        
+        System.out.printf("3° Nome: %s", nomes[2]);
+        System.out.printf("7° Nome: %s", nomes[6]);
+        System.out.printf("9° Nome: %s", nomes[8]);
+    }
+    
+}
+/**
+ * 
+ */
