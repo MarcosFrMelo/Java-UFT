a3d0593af70c93240dc189fe973438dbafa00101
MarcosFrMelo <143053350+MarcosFrMelo@users.noreply.github.com>
2024-04-09 19:23:51 -0300

    Update NotaAluno.java

--- AtividadesSala/NotaAluno.java
+++ AtividadesSala/NotaAluno.java
@@ -1,15 +1,19 @@
 package AtividadesSala;
+//alt shift "o" para importar automaticamente 
+import java.util.Scanner;
 
 public class NotaAluno {
     public static void main(String[] args) {
-        int nota = 9;
+        int nota = 6;
+        Scanner scanner = new Scanner(System.in);
+        String resultado = nota >=7 ? "Aprovado" : nota < 5 ? "Reprovado" : "Recuperação";
+        System.out.println(resultado);
+        System.out.println("asd" + "asd");
 
-        if (nota >= 7)
-            System.out.println("Aprovado");
-        else if(nota < 5)
-            System.out.println("Reprovado"); 
-        else
-            System.out.println("Recuperação");
-            
+        String nomes[] = {"MArcos", "Italo", "jhennifer", "Luis", "Thomaz", "Guilherme", "Brito"};
+        
+        for(int x = 0; x<nomes.length; x++){
+            System.out.println("Nome do indice " + x + " é " + nomes[x]);
+        }
     }
 }
