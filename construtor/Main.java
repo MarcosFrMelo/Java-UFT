5d084bec62f17448e2200b16cd9b82f441203fab
MarcosFrMelo <143053350+MarcosFrMelo@users.noreply.github.com>
2024-04-23 20:18:51 -0300

    att

---
+++ construtor/Main.java
@@ -0,0 +1,7 @@
+package construtor;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa marcos = new Pessoa("Marcos Freire de Melo", "072.459.281-42", "casa da mãe joana");
+    }
+}
---
+++ construtor/Pessoa.java
@@ -0,0 +1,15 @@
+package construtor;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+    public Pessoa(String nome, String cpf, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    
+}
